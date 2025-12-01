b1745dd06442c712274f2aa820c42f290fe5acdc
AgainTri412 <nullptragaintri@gmail.com>
2025-12-01 05:59:00 -0500

    Add contract template

---
+++ Classes/contract/GameConfig.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "GameStructs.h"
+#include <map>
+
+// TODO(szj): Modify the values here where needed.
+class GameConfig {
+public:
+    // Singleton access
+    static GameConfig* GetInstance();
+
+    // The Lookup Functions
+    UnitStats GetTroopStats(TroopType type, int level) {
+        UnitStats stats;
+        if (type == TroopType::kGiant) {
+            stats.max_hp_ = 300 + (level * 50);  // Level scaling
+            stats.damage_per_shot_ = 11;
+            stats.move_speed_ = 1.5f;            // Slow
+            stats.housing_space_ = 5;
+            stats.range_ = 1.0f;                 // Melee
+            stats.favorite_target_ = BuildingType::kCannon;  // Or generic DEFENSE
+        }
+        // ... define others ...
+        return stats;
+    }
+
+    BuildingStats GetBuildingStats(BuildingType type, int level) {
+        BuildingStats stats;
+        if (type == BuildingType::kTownHall) {
+            stats.width_ = 4;   // Size on Grid
+            stats.height_ = 4;
+            stats.max_hp_ = 1500;
+        } else if (type == BuildingType::kWall) {
+            stats.width_ = 1;
+            stats.height_ = 1;
+            stats.max_hp_ = 500;
+        }
+        return stats;
+    }
+    
+    // Resource Costs
+    int GetUpgradeCost(BuildingType type, int level) {
+        // Return gold/elixir cost
+        return 500 * level; 
+    }
+};
