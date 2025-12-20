4179b49bfdf5ae833f18f73ddbd3ee003b323b03
revival-of-hope <2740890377@qq.com>
2025-12-20 17:02:30 +0800

    1

--- Resources/maps/grass.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="grass" tilewidth="16" tileheight="16" spacing="20" margin="20" tilecount="660" columns="30">
- <image source="../../../../test/Resources/origin map.png" width="1080" height="810"/>
-</tileset>
